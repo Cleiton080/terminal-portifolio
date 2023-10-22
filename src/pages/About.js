a7ab9aa3346c2fe5b52f41c29033c4e7f6e0a68d
cleiton080 <cleitonpessoa53@hotmail.com>
2023-10-22 20:20:58 -0300

    refactor: change about image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,14 @@
 import {
   Box,
+  Grid,
   List,
   ListItem,
   ListItemText,
-  Stack,
   Typography,
 } from "@mui/material";
-import me from "../assets/ascii/me";
-import { transformAsciiImage } from "../common/utils";
+import me from "../assets/images/me.png";
 
 export default function About() {
-  const image = transformAsciiImage(me, 2);
-
   const specializationList = [
     "* Web Application Development",
     "* Software Architecture",
@@ -20,49 +17,48 @@ export default function About() {
   ];
 
   return (
-    <Stack
-      spacing={{ xs: 1, sm: 2 }}
-      direction="row"
-      useFlexGap
-      flexWrap="nowrap"
-    >
-      {image}
-      <Box>
-        <Typography gutterBottom>
-          Hello, I'm Cleiton, a software developer passionate about translating
-          lines of code into innovative solutions, and currently, I'm exploring
-          the exciting world of DevOps. With over 4 years of experience in the
-          technology industry, I've tackled challenging projects that have
-          allowed me to grow and evolve continuously.
-        </Typography>
-        <Typography gutterBottom>
-          My professional journey has been marked by an unwavering pursuit of
-          excellence in software development. My main areas of expertise
-          include:
-        </Typography>
-        <List>
-          {specializationList.map((specialization) => (
-            <ListItem>
-              <ListItemText primary={specialization} />
-            </ListItem>
-          ))}
-        </List>
-        <Typography gutterBottom>
-          My approach to software development is based on problem-solving and a
-          constant quest for improvement. I believe that technology has the
-          power to positively impact people's lives, and I'm committed to
-          creating solutions that are not only functional but also intuitive and
-          enjoyable to use.
-        </Typography>
-        <Typography gutterBottom>
-          I believe that the success of any project depends not only on the code
-          but also on how people work together.
-        </Typography>
-        <Typography gutterBottom>
-          I'm always eager to stay updated on the latest industry trends and
-          take on new challenges.
-        </Typography>
-      </Box>
-    </Stack>
+    <Grid container spacing={2} direction="row" flexWrap="nowrap">
+      <Grid item md={4}>
+        <img src={me} alt="A ascii photo of myself" width="100%"/>
+      </Grid>
+      <Grid item md={7}>
+        <Box>
+          <Typography gutterBottom>
+            Hello, I'm Cleiton, a software developer passionate about
+            translating lines of code into innovative solutions, and currently,
+            I'm exploring the exciting world of DevOps. With over 4 years of
+            experience in the technology industry, I've tackled challenging
+            projects that have allowed me to grow and evolve continuously.
+          </Typography>
+          <Typography gutterBottom>
+            My professional journey has been marked by an unwavering pursuit of
+            excellence in software development. My main areas of expertise
+            include:
+          </Typography>
+          <List>
+            {specializationList.map((specialization) => (
+              <ListItem>
+                <ListItemText primary={specialization} />
+              </ListItem>
+            ))}
+          </List>
+          <Typography gutterBottom>
+            My approach to software development is based on problem-solving and
+            a constant quest for improvement. I believe that technology has the
+            power to positively impact people's lives, and I'm committed to
+            creating solutions that are not only functional but also intuitive
+            and enjoyable to use.
+          </Typography>
+          <Typography gutterBottom>
+            I believe that the success of any project depends not only on the
+            code but also on how people work together.
+          </Typography>
+          <Typography gutterBottom>
+            I'm always eager to stay updated on the latest industry trends and
+            take on new challenges.
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
